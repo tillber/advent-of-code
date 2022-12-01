e8eb60d350525bcfed18476a3565760cc9d11f8f
tillber <tillber@kth.se>
2022-12-01 21:15:46 +0100

    Specify namespace

--- 2022/first/main.clj
+++ 2022/first/main.clj
@@ -1,9 +1,8 @@
-(require '[clojure.string :as str])
+(ns first.core 
+(:require '[clojure.string :as str]))
 
 (defn main []
 (def elfs (str/split (slurp "calories.txt") #"\n\r\n\r"))
 (def sums (for [elf elfs] (apply + (map #(Integer/parseInt %) (str/split elf #"\n\r")))))
 (println (apply max sums))
 )
-
-(main)
