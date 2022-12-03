3da31249f2ce4eb8e99e3a10b098fac1b32b76ec
tillber <tillber@kth.se>
2022-12-03 19:15:24 +0100

    Minor refactor

--- 2022/first/main.py
+++ 2022/first/main.py
@@ -3,11 +3,11 @@ calories = open("calories.txt", "r").read()
 
 def main():
     elfs = list(map(lambda elf: list(map(int, elf.split("\n"))), calories.split("\n\n")))
-    elfs_calories = list(map(sum, elfs))
+    elfs_calories = sorted(list(map(sum, elfs)), reverse=True)
 
     print(f"The elf carrying the most calories carries a total of {max(elfs_calories)} calories!")
 
-    total_cals = sum(sorted(elfs_calories, reverse=True)[:3])
+    total_cals = sum(elfs_calories[:3])
     print(f"The top three elves carrying the most calories are carrying {total_cals} calories in total!")
 
 if __name__ == "__main__":
