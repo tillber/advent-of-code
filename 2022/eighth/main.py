c08b8d3cb878a94afd2084d883f39d3751d50baa
Martin Tillberg <martin.tillberg@tv4.se>
2022-12-08 14:43:49 +0100

    naïve part1 solution

--- 2022/eighth/main.py
+++ 2022/eighth/main.py
@@ -3,8 +3,6 @@ trees = list(map(lambda x: list(x), forrest.split("\n")))
 
 
 def main():
-    print(trees)
-
     visible_trees = (len(trees[0]) + (len(trees) - 2)) * 2
 
     for i in range(1, (len(trees) - 1)):
@@ -12,48 +10,37 @@ def main():
             if is_visible(i, j):
                 visible_trees += 1
 
-    print(visible_trees)
+    print(f"Number of visible trees: {visible_trees}")
 
 
 def is_visible(y, x) -> bool:
     tree = trees[y][x]
-    print(tree)
 
-    hidden = True
+    hidden_left = False
     for k in range(x, 0, -1):  # left
-        tr = trees[y][k - 1]
-        p_tr = trees[y][k]
-        if tr >= p_tr:
-            print(tr)
-            hidden = True
-            print("hidden left")
+        if trees[y][k - 1] >= tree:
+            hidden_left = True
             break
 
+    hidden_right = False
     for k in range(x, len(trees[y]) - 1, 1):  # right
-        tr = trees[y][k + 1]
-        p_tr = trees[y][k]
-        if tr >= p_tr:
-            hidden = True
-            print("hidden right")
+        if trees[y][k + 1] >= tree:
+            hidden_right = True
             break
 
+    hidden_top = False
     for k in range(y, 0, -1):  # top
-        tr = trees[k - 1][x]
-        p_tr = trees[k][x]
-        if tr >= p_tr:
-            hidden = True
-            print("hidden top")
+        if trees[k - 1][x] >= tree:
+            hidden_top = True
             break
 
+    hidden_bottom = False
     for k in range(y, len(trees) - 1, 1):  # bottom
-        tr = trees[k + 1][x]
-        p_tr = trees[k][x]
-        if tr >= p_tr:
-            hidden = True
-            print("hidden bottom")
+        if trees[k + 1][x] >= tree:
+            hidden_bottom = True
             break
 
-    return not hidden
+    return not (hidden_right and hidden_left and hidden_top and hidden_bottom)
 
 
 if __name__ == "__main__":
