26d63f3306e332dba52d64594c12849909df7dc8
Martin Tillberg <martin.tillberg@tv4.se>
2022-12-08 16:54:44 +0100

    begin calc of scenic score

--- 2022/eighth/main.py
+++ 2022/eighth/main.py
@@ -1,4 +1,5 @@
 forrest = open("forrest.txt", "r").read()
+
 trees = list(map(lambda x: list(x), forrest.split("\n")))
 
 
@@ -7,6 +8,7 @@ def main():
 
     for i in range(1, (len(trees) - 1)):
         for j in range(1, (len(trees[i]) - 1)):
+            calculate_scenic_score(i, j)
             if is_visible(i, j):
                 visible_trees += 1
 
@@ -40,6 +42,42 @@ def is_visible(y, x) -> bool:
             hidden_bottom = True
             break
 
+    return not (hidden_right and hidden_left and hidden_top and hidden_bottom)
+
+
+def calculate_scenic_score(y, x) -> bool:
+    tree = trees[y][x]
+    scenic_factors = []
+
+    hidden_left = False
+    for k in range(x, 0, -1):  # left
+        if trees[y][k - 1] >= tree:
+            distance = x - (k - 1)
+            scenic_factors.append(distance)
+            break
+
+    hidden_right = False
+    for k in range(x, len(trees[y]) - 1, 1):  # right
+        if trees[y][k + 1] >= tree:
+            distance = x - (k + 1)
+            scenic_factors.append(distance)
+            break
+
+    hidden_top = False
+    for k in range(y, 0, -1):  # top
+        if trees[k - 1][x] >= tree:
+            distance = y - (k - 1)
+            scenic_factors.append(distance)
+            break
+
+    hidden_bottom = False
+    for k in range(y, len(trees) - 1, 1):  # bottom
+        if trees[k + 1][x] >= tree:
+            distance = y - (k + 1)
+            scenic_factors.append(distance)
+            break
+
+    print(scenic_factors)
     return not (hidden_right and hidden_left and hidden_top and hidden_bottom)
 
 
