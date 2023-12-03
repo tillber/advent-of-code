91789dfd940ba26391c6ef3a3f4f50255ff754ff
tillber <tillber@kth.se>
2023-12-03 19:21:51 +0100

    part two

--- 2023/first/main.py
+++ 2023/first/main.py
@@ -1,22 +1,44 @@
 calibration = open("calibration.txt", "r").read()
 calibration_lines = calibration.split("\n")
 
+digits = {'one': '1', 'two': '2', 'three': '3', 'four': '4',
+          'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+
 sum = 0
 
 for line in calibration_lines:
-    last = ""
-    first = ""
+    print(f"line: {line}")
 
-    for c in line:
+    last = None
+    first = None
+
+    for i, c in enumerate(line):
         if c.isnumeric():
             if not first:
-                first = c
+                first = (i, c)
             else:
-                last = c
+                last = (i, c)
 
-    if first and last:
-        sum += int(first + last)
-    else:
-        sum += int(first + first)
+    if not last:
+        last = first
+
+    # found by digits
+    print(f"first: {first}, last: {last}")
+
+    for d in digits.keys():
+        lfind = line.find(d)
+        rfind = line.rfind(d)
+
+        if lfind >= 0 and (not first or lfind < first[0]):  # new 'first' found
+            first = (lfind, digits[d])
+
+        if rfind >= 0 and (not last or rfind > last[0]):  # new 'last' found
+            last = (rfind, digits[d])
+
+    # found by name (updated)
+    print(f"updated first: {first}, last: {last}")
+
+    print(int(first[1] + last[1]))
+    sum += int(first[1] + last[1])
 
 print(sum)
